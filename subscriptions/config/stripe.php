f472dd2bb1d1eca812855b52ab173e5a622fd52b
Saud <ashfaqsaud@gmail.com>
2025-05-21 21:18:44 +0500

    keys commented out

--- subscriptions/config/stripe.php
+++ subscriptions/config/stripe.php
@@ -2,8 +2,8 @@
 // config/stripe.php
 
 // Stripe API keys (replace with your actual keys)
-define('STRIPE_SECRET_KEY', 'sk_test_51RNssO2MOP7tABA3D0DJe2aPFz0KldaqogwMNLipjLf3yA1Om3pPLKMvdenMi5UfHP5c82WKkFQrr7huVIOdKk2j00mcK5JJY7'); // Your Stripe secret key
-define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51RNssO2MOP7tABA33LKW03rHhhpKin9dp0N2zvz4DnKU9bFa8njcvvunqVbnBl9pnSvbg4VRtG3l5OXmSV0eNBhI00KdTy3jpZ'); // Your Stripe publishable key
+//define('STRIPE_SECRET_KEY', 'sk_test_51RNssO2MOP7tABA3D0DJe2aPFz0KldaqogwMNLipjLf3yA1Om3pPLKMvdenMi5UfHP5c82WKkFQrr7huVIOdKk2j00mcK5JJY7'); // Your Stripe secret key
+//define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51RNssO2MOP7tABA33LKW03rHhhpKin9dp0N2zvz4DnKU9bFa8njcvvunqVbnBl9pnSvbg4VRtG3l5OXmSV0eNBhI00KdTy3jpZ'); // Your Stripe publishable key
 
 // Include the Stripe PHP SDK
 require_once __DIR__ . '/../stripe-php/init.php';
